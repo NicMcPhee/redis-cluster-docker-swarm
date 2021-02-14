11794b808198920b24ec24f4535342665a7c97ae
Nic McPhee <mcphee@morris.umn.edu>
2021-02-14 16:56:47 -0600

    Attempt to fix problem accessing Redis
    The Flask clients weren't successfully contacting a Redis client, sometimes at all, and other times because they'd get a "slave" when they'd need a "master". I think this might fix that?
    
    I also added a second route, so we can just get the count without updating it, and increment and return the count. The response now includes the name and ID, although I'm not sure _what_ they're the name and ID of, and whether they will be useful.

--- flask-redis-counter/app.py
+++ flask-redis-counter/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from redis import Redis
+from redis.sentinel import Sentinel
 
 app = Flask(__name__)
-redis = Redis(host='redismaster', port=6379)
+sentinel = Sentinel(['localhost', 26379], socket_timeout=0.1)
+master = sentinel.master_for('redismaster', socket_timeout=0.1)
+slave = sentinel.slave_for('redismaster', socket_timeout=0.1)
 
 @app.route('/')
-def hello():
-    count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+def get_count():
+    count = slave.get('hits')
+    return 'The current counter value is {} as reported by {} with ID {}.\n'.format(count, slave.client_getname(), slave.client_id())
+
+@app.route('/inc')
+def inc_count():
+    count = master.incr('hits')
+    return 'After incrementing it, the counter value is {} as report by {} with ID {}.\n'.format(count, master.client_getname(), master.client_id())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
