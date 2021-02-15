0ee86180ff0f53ff7fc486b78a2ef94efdad9c77
Nic McPhee <mcphee@morris.umn.edu>
2021-02-14 22:59:55 -0600

    Fix problems with accessing Redis nodes
    It took me _way_ too long to figure out how to get all the nodes to connect up, but now I think things are happy and this will hopefully work.

--- flask-redis-counter/app.py
+++ flask-redis-counter/app.py
@@ -1,22 +1,26 @@
+import platform
 from flask import Flask
 from redis import Redis
 from redis.sentinel import Sentinel
 
+hostname = platform.node()
+
 app = Flask(__name__)
 
-sentinel = Sentinel([('localhost', 26379)], socket_timeout=0.1)
+sentinel = Sentinel([('redis-sentinel', 26379)], socket_timeout=0.1)
 master = sentinel.master_for('redismaster', socket_timeout=0.1)
 slave = sentinel.slave_for('redismaster', socket_timeout=0.1)
 
 @app.route('/inc')
 def inc_count():
     count = master.incr('hits')
-    return 'After incrementing it, the counter value is {} as report by {} with ID {}.\n'.format(count, master.client_getname(), master.client_id())
+    (master_ip, port) = sentinel.discover_master('redismaster')
+    return 'After incrementing it, the counter value is {}.\nThe Redis IP is {}.\nThe Python host is {}.\n'.format(count, master_ip, hostname)
 
 @app.route('/')
 def get_count():
     count = slave.get('hits')
-    return 'The current counter value is {} as reported by {} with ID {}.\n'.format(count, slave.client_getname(), slave.client_id())
+    return 'The current counter value is {}.\nThe Python host is {}.\n'.format(count, hostname)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
